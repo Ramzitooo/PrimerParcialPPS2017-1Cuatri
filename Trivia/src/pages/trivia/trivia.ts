ef5af825bea0b3148e676a43e3324d965e4883ba
Ramzitooo <osmar_sc2@live.com>
2017-04-18 20:29:10 -0300

    -Agregado de algunos comentarios en mi pagina trivia. -Eliminacion de datos o variables innecesarios.

--- Trivia/src/pages/trivia/trivia.ts
+++ Trivia/src/pages/trivia/trivia.ts
@@ -12,21 +12,21 @@ import { ResultadosPage } from '../resultados/resultados';//AGREGADO
 export class TriviaPage 
 {
     user :Usuario;//CREO UN OBJETO DE TIPO USUARIO.
-    preguntas: Array <Preguntas>;
+    preguntas: Array <Preguntas>;//CREO UN ARRAY DE TIPO PREGUNTAS.
 
     pregunta1:Preguntas = new Preguntas();
     pregunta2:Preguntas = new Preguntas();
     pregunta3:Preguntas = new Preguntas();
 
-    puntajePartida:number;
+    puntajePartida:number;//PARA EL PUNTAJE DE LA PARTIDA ACTUAL.
 
-    mipregunta:number;
+    mipregunta:number;//LA PREGUNTA ACTUAL
 
     constructor(public navCtrl: NavController,public navParams: NavParams,public alertCrtl:AlertController)
     {
-        this.user = navParams.get("Usuario");
-
-       this.pregunta1.pregunta="Quien es el mayor goleador de la seleccion argentina de futbol?";
+        this.user = navParams.get("Usuario");//SETEO EL USUARIO LOGEADO.
+        //SETEO MIS PREGUNTAS CON DIFERENTES RESPUESTAS Y 1 CORRECTA.
+        this.pregunta1.pregunta="Quien es el mayor goleador de la seleccion argentina de futbol?";
         this.pregunta1.respuestas=["Messi","PepeSand","Wanchope"];
         this.pregunta1.correcta=0;
 
@@ -38,56 +38,54 @@ export class TriviaPage
         this.pregunta3.respuestas=["Scioli","Stolbizer","Macri"];
         this.pregunta3.correcta=2;
 
-        this.preguntas = new Array<Preguntas>();
-
-        /*this.preguntas.push(new Preguntas("1+1", ["1", "2", "3"], 1));
-        this.preguntas.push(new Preguntas("1+2", ["1", "2", "3"], 2));
-        this.preguntas.push(new Preguntas("1+3", ["4", "2", "3"], 0));*/
-
+        this.preguntas = new Array<Preguntas>();//INSTANCIO MI ARRAY DE PREGUNTAS. FUNDAMENTAL...
+        //AGREGO LAS PREGUNTAS A MI ARRAY
         this.preguntas.push(this.pregunta1);
         this.preguntas.push(this.pregunta2);
         this.preguntas.push(this.pregunta3);
-    
+        //CONTADOR EN 0 
         this.puntajePartida=0;
         this.mipregunta=0;
     }
     Aceptar(opc:number):void
     {
-        if(opc == this.preguntas[this.mipregunta].correcta)
+        if(opc == this.preguntas[this.mipregunta].correcta)//SI EL BOTON APRETADO ES IGUAL A MI PREGUNTA ACTUAL CON RESPUESTA CORRECTA.
         {
+            //EN CASI DE SER VERDAD AGREGO MI PUNTAJE DE 100 POR PREGUNTA CORRECTA.
             this.user.puntuacion=this.user.puntuacion+100;
             this.puntajePartida=this.puntajePartida+100;
-            this.user.correctas++;
+            this.user.correctas++;//CUENTO LAS CORRECTAS EN EL OBJETO USUARIO
             
             console.log("Pregunta "+this.mipregunta+" contesto correctamente!");
         }
         else
         {
             
-            this.user.incorrectas++;
+            this.user.incorrectas++;//CUENTO LAS INCORRECTAS EN EL OBJETO USUARIO
             console.log("Pregunta "+this.mipregunta+" contesto incorrectamente!");
         }
 
-        if(this.mipregunta!=2)
+        if(this.mipregunta!=2)//EN CASO DE NO LLEGAR A LA ULTIMA PREGUNTA.
         {
-            this.mipregunta++;
+            this.mipregunta++;//PASA CON LA SIGUENTE PREGUNTA.
             console.log("Paso a la siguente pregunta.");
         }
         else
         {
+            //SI ENTRA VOY A LA PANTALLA DE PUNTUACION Y MANDA LOS DATOS NECESARIO PARA LAS MISMAS.
             this.user.partidas++;
             console.log("Se acabo el juego!");
             console.log(this.user);
-            this.navCtrl.setRoot(ResultadosPage, 
-            {Usuario : this.user,
-            Puntaje : this.puntajePartida,}, 
+            this.navCtrl.setRoot(ResultadosPage, //INDICO QUE PAGINA VOY.
+            {Usuario : this.user,//MANDA EL USUARIO YA SETEADO.
+            Puntaje : this.puntajePartida,}, //MANDO EL PUNTAJE DE LA PARTIDA ACTUAL.
             {animate: true, 
             direction: "forward"});    
         }
     }
 
 }
-export class Preguntas
+export class Preguntas//CREO UN OBJETO DE TIPO PREGUNTAS.
 {
     public pregunta : string;
     public respuestas : Array <string>;
